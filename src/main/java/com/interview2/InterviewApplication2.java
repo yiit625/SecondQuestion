523cddb6132cd9d8ac34a3406a7b91a414fb220c
yigitsahin <yigit.sahin@denk-it.com>
2021-10-19 23:55:12 +0300

    Clean Code

--- src/main/java/com/interview2/InterviewApplication2.java
+++ src/main/java/com/interview2/InterviewApplication2.java
@@ -10,43 +10,43 @@ public class InterviewApplication2 {
         Scanner numberOfBooks = new Scanner(System.in);
         System.out.println("Lütfen kitap sayısını giriniz");
         int n = numberOfBooks.nextInt();
-        if(n>10 || n<1) {
+            if (n>10 || n<1) {
             System.out.println("Kitap sayısı 1'den küçük 10'dan büyük olamaz!");
-        }
-        else {
+            }
+            else {
             int[] price = new int[n];
             boolean isValid= true;
             Scanner priceOfBooks = new Scanner(System.in);
             System.out.println("Lütfen Kitap ücretlerini giriniz");
-            for(int i=0; i<n; i++) {
-                price[i]=priceOfBooks.nextInt();
-                if(price[i]> 100 || price[i]<1){
+                for (int i=0; i<n; i++) {
+                    price[i]=priceOfBooks.nextInt();
+                    if (price[i]> 100 || price[i]<1) {
                     isValid = false;
+                    }
+                }
+                if (isValid) {
+                Scanner budgetInput = new Scanner(System.in);
+                System.out.println("Lütfen Bütçeyi giriniz");
+                int budget = budgetInput.nextInt();  // Read user input
+                    if (budget<1 ||  budget> 1000) {
+                        System.out.println("Bütçe 1'den küçük 1000'den büyük olamaz");
+                    }
+                    else {
+                    System.out.println("Bir seferde alınabilecek max kitap sayısı :" + getMaxBooks(price,budget));
+                    }
                 }
             }
-            if(isValid) {
-            Scanner budgetInput = new Scanner(System.in);
-            System.out.println("Lütfen Bütçeyi giriniz");
-            int budget = budgetInput.nextInt();  // Read user input
-            if(budget<1 ||  budget> 1000) {
-                System.out.println("Bütçe 1'den küçük 1000'den büyük olamaz");
-            }
-            else {
-                System.out.println("Bir seferde alınabilecek max kitap sayısı :" + getMaxBooks(price,budget));
-            }
-            }
-        }
         }
         catch (Exception e) {
-            System.out.println("Please Enter Only Numbers");
+            System.out.println("Lütfen sadece sayı giriniz!");
         }
     }
     public static int getMaxBooks (int[] price, int budget) {
         int maxBooksNumber=0;
         Arrays.sort(price);
-        for(int i=0; i< price.length; i++) {
-            if(budget > price[i]){
-                budget = budget-price[i];
+        for (int j : price) {
+            if (budget > j) {
+                budget = budget - j;
                 maxBooksNumber++;
             }
         }
